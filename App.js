761ef2cce34fede24da9bc72cbcb98f1a28c933a
Gauravmishra <iamgauravdbg@gmail.com>
2024-10-06 18:16:09 +0530

    multiple route handler

--- App.js
+++ App.js
@@ -1,61 +1,18 @@
 const expree=require("express");
 const app=expree();
 // .................2
+ app.use('/user',(req,res,next)=>{
+  console.log("1st route handler..........")
+  next();
 
-  app.get("/" , (req,res)=>{
-    res.send("This is my first Server");
-  console.log("This is my first Server of console");
-});
-app.get("/hello" , (req,res)=>{
-  res.send("This is my hello router Server..........");
-  console.log("This is my first Server of hello route");
-});
-app.get("/test" , (req,res)=>{
-  res.send("This is my hello router Server..........");
-  console.log("This is my first Server of hello route");
-}) 
-  
-// .........................1
+res.send("first route")
+},
 
-app.get("/user",(req,res)=>{
-  res.send(
-    {first:"gaurav",
-      last:"mishra"
-    } )
-});
-app.post("/user",(req,res)=>{
-  res.send("data save on database..")
-})
-app.delete("/user",(req,res)=>{
-  res.send("data delete on database..")
-})
-// ..................3
-app.use('/ab?c',(req,res)=>{
-  res.send("opptional channing")
-})
-// ...............4      rexex
-app.use(/.*fly$/,(req,res)=>{
-res.send("priffix any thing start but suffix will fly")
-console.log(req.query);
-})
-// ...............5
-app.use("/user",(req,res)=>{
-  res.send({
-    name:"RITU",age:22
-  })
-  console.log(req.query)
-})
-// ...............6
-app.get("user/:userid/:username/",(req,res)=>{
-  res.send({
-    name:"RITU",age:22
-  })
-  console.log(req.params);
-})
-// ............7
-app.use('/user',(req,res)=>{
-  
-})
+(req,res)=>{
+  res.send("second router handler");
+  console.log("2nd route handler............")
+}
+)
 app.listen(8000,()=>{
   console.log("Server connecetion successfull.............")
 })
