74eaa26f5484df97e3482fcdc8862cc73c29c6cb
Gauravmishra <iamgauravdbg@gmail.com>
2024-10-06 17:45:32 +0530

    rezex

--- App.js
+++ App.js
@@ -1,9 +1,5 @@
 const expree=require("express");
 const app=expree();
-
-
-
-
 // .................2
 
   app.get("/" , (req,res)=>{
@@ -32,6 +28,33 @@ app.post("/user",(req,res)=>{
 })
 app.delete("/user",(req,res)=>{
   res.send("data delete on database..")
+})
+// ..................3
+app.use('/ab?c',(req,res)=>{
+  res.send("opptional channing")
+})
+// ...............4      rexex
+app.use(/.*fly$/,(req,res)=>{
+res.send("priffix any thing start but suffix will fly")
+console.log(req.query);
+})
+// ...............5
+app.use("/user",(req,res)=>{
+  res.send({
+    name:"RITU",age:22
+  })
+  console.log(req.query)
+})
+// ...............6
+app.get("user/:userid/:username/",(req,res)=>{
+  res.send({
+    name:"RITU",age:22
+  })
+  console.log(req.params);
+})
+// ............7
+app.use('/user',(req,res)=>{
+  
 })
 app.listen(8000,()=>{
   console.log("Server connecetion successfull.............")
